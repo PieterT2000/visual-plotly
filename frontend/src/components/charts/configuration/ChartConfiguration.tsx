d06632b014b96c13e3989050fa3bcb582ccc8e98
Pieter Van Tuijl <pietervantuijl2000@gmail.com>
2024-03-29 10:17:10 +0000

    add example file + update readme with future work suggestions

--- frontend/src/components/charts/configuration/ChartConfiguration.tsx
+++ frontend/src/components/charts/configuration/ChartConfiguration.tsx
@@ -1,14 +1,17 @@
 import Canvas from "src/components/canvas/Canvas";
 import ChartConfigFields from "./ChartConfigFields";
 import TraceConfigFields from "./TraceConfigFields";
+import { ScrollArea } from "src/components/ui/scroll-area";
 
 const ChartConfiguration = () => {
   return (
-    <div className="flex items-center h-full">
-      <div className="py-8 space-y-8 w-[400px] lg:w-[500px] bg-cgray h-full flex flex-col">
-        <ChartConfigFields />
-        <TraceConfigFields />
-      </div>
+    <div className="flex items-center">
+      <ScrollArea className="h-screen">
+        <div className="py-8 space-y-8 w-[400px] lg:w-[500px] bg-cgray min-h-screen flex flex-col">
+          <ChartConfigFields />
+          <TraceConfigFields />
+        </div>
+      </ScrollArea>
       <div className="flex grow flex-col justify-center h-screen">
         <Canvas />
       </div>
--- frontend/src/components/upload/Dropzone.tsx
+++ frontend/src/components/upload/Dropzone.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { FileUploader, FileCard, MimeType, FileRejection } from "evergreen-ui";
 import { cn } from "../../utils";
 import { Button } from "../ui/button";
+import { ArrowRightCircle } from "lucide-react";
 
 interface DropzoneProps {
   onContinue: (urls: string[]) => void;
@@ -25,6 +26,19 @@ export function Dropzone({ onContinue, className = "" }: DropzoneProps) {
   const handleContinue = () => {
     const urls = validFiles.map((file) => URL.createObjectURL(file));
     onContinue(urls);
+  };
+
+  const handleUseExampleFile = () => {
+    // load example file from public folder
+    fetch("/example.json")
+      .then((response) => response.blob())
+      .then((blob) => {
+        const file = new File([blob], "example.json", {
+          type: "application/json",
+        });
+        const urls = [URL.createObjectURL(file)];
+        onContinue(urls);
+      });
   };
 
   const validateJson = (file: File) => {
@@ -59,7 +73,7 @@ export function Dropzone({ onContinue, className = "" }: DropzoneProps) {
   return (
     <div className="w-full md:max-w-[800px] ">
       <div className="text-center">
-        <h3>Upload File</h3>
+        <h3 className="font-semibold">Upload File</h3>
         <p className="text-sm text-gray-500">
           You can upload 1 file. The file must have a .json extension.
         </p>
@@ -92,8 +106,21 @@ export function Dropzone({ onContinue, className = "" }: DropzoneProps) {
         }}
       />
       {!!validFiles.length && (
-          <div className="flex justify-center">
+        <div className="flex justify-center">
           <Button onClick={handleContinue}>Continue</Button>
+        </div>
+      )}
+      {files.length == 0 && (
+        <div className="flex justify-center items-center space-x-4">
+          <div className="font-semibold">Or...</div>
+          <Button
+            variant={"outline"}
+            className="border-primary text-primary hover:bg-primary hover:text-white space-x-2"
+            onClick={handleUseExampleFile}
+          >
+            <span>Use example file</span>
+            <ArrowRightCircle size={20} />
+          </Button>
         </div>
       )}
     </div>
